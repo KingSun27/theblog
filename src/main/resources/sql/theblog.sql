de3f9d06d02219f0fc23ab4a7c8bc45ae983466b
KingSun27 <270403579@qq.com>
2017-09-20 08:38:32 +0800

    update

---
+++ src/main/resources/sql/theblog.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `blog` (
+  `id` int(11) NOT NULL,
+  `title` varchar(45) DEFAULT NULL COMMENT '标题',
+  `content` varchar(4000) DEFAULT NULL COMMENT '内容',
+  `description` varchar(100) DEFAULT NULL,
+  `create_time` timestamp(6) NULL DEFAULT NULL,
+  `update_time` timestamp(6) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
